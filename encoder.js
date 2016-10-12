26b7c9edfa8982e81d1f19a6ccf7be88eed6919a
kyle lao <kylelao3201@gmail.com>
2016-10-12 09:14:53 -0700

    did part 4

--- encoder.js
+++ encoder.js
@@ -1,16 +1,38 @@
+//convert numbers to letters
 var letterConversion = {
-	"A": 0,
-	"B": 1,
-	"C": 2,
-	"D": 3,
-	"E": 4,
-	"F": 5,
-	"G": 6,
-	"H": 7,
-	"I": 8,
-	"J": 9
+	65: "a",
+	66: "b",
+	67: "c",
+	68: "d",
+	69: "e",
+	70: "f",
+	71: "g",
+	72: "h",
+	73: "i",
+	74: "j",
+  75: "k",
+  76: "l",
+  77: "m",
+  78: "n",
+  79: "o",
+  80: "p",
+  81: "q",
+  82: "r",
+  83: "s",
+  84: "t",
+  85: "u",
+  86: "v",
+  87: "w",
+  88: "x",
+  89: "y",
+  90: "z"
 }
 
 $("#inputArea").keydown(function(e){
+  letters[e.keyCode]
+});
 
+$("input[type='radio']").click(function(){
+$("input:checked").prop('checked', false);
+$(this).porp('checked',true);
 });
