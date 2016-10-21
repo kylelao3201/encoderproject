516cc25f7a2952ca6ffa61bdfb5f8f9e80628e6f
kyle lao <kylelao3201@gmail.com>
2016-10-21 08:08:49 -0700

    part 7

--- encoder.js
+++ encoder.js
@@ -1,5 +1,6 @@
 //convert numbers to letters
 var letters = {
+	64: "z",
 	65: "a",
 	66: "b",
 	67: "c",
@@ -25,21 +26,46 @@ var letters = {
   87: "w",
   88: "x",
   89: "y",
-  90: "z"
+  90: "z",
+	91: "a",
+	31: "",
+	32: ""
 }
-$("#inputArea").keydown(function(e){
-	if($("#input:checked").val() == "echo"){
-  $("#textArea").append(letters[e.keyCode]);
-}
-	if($("#input:checked").val() == "Caesar cipher"){
-  $("#textArea").append(letters[e.keyCode - 1]);
-}
-	if($("#input:checked").val() == ""){
-  $("#textArea").append(letters[e.keyCode]);
-}
-});
 
 $("input[type='radio']").click(function(){
 $("input:checked").prop('checked', false);
-$(this).porp('checked',true);
+$(this).prop('checked',true);
+});
+
+$("#inputArea").keydown(function(e){
+
+if($("input:checked").val() == "echo"){
+  $("#textArea").append(letters[e.keyCode]);
+}
+
+if($("input:checked").val() == "caesar cipher"){
+$("#textArea").append(letters[e.keyCode - 1]);
+}
+
+if($("input:checked").val() == "heiroglyphics"){
+  $("#textArea").append("<img src = 'images/heiroglyphics/" + letters[e.keyCode] + ".gif'>");
+}
+
+if($("input:checked").val() == "runes"){
+$("#textArea").append("<img src = 'images/runes/" + letters[e.keycode] + ".gif'>");
+}
+
+});
+
+$("#translationInputText").keydown(function(e){
+	if(e.keyCode == 13){
+	//1. grab the value of the input text box
+  //$("#translationInputText").val()
+	//2. turn the string in the text box into an array
+	// inputTextBoxString.split("")
+	//3. loop through the array and traslate each letter
+
+	//4. handy way to go from letter to number: charCodeAt(0)
+
+	}
 });
